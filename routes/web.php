ca17536093b3220bef1e94e38991e4ca8bbfb4c3
Shafira <sucimarv@gmail.com>
2026-05-18 17:43:46 +0700

    web php
    Routing Laravel

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::get('/matkul', function () {
+    return view('matkul.index');
+});
+
+Route::get('/quiz', function () {
+    return view('quiz.index');
+});
+
+Route::get('/result', function () {
+    return view('result.index');
+});
+
+Route::get('/progress', function () {
+    return view('progress.index');
 });
