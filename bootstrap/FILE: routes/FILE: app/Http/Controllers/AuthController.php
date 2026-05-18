a6c922fd4c08ae2e443331e0011e39c10f11a8c8
zaza082231 <115444850+zaza082231@users.noreply.github.com>
2026-05-18 18:14:01 +0700

    Add AuthController for user registration and login

---
+++ bootstrap/FILE: routes/FILE: app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return response()->json([
+            'message' => 'Register berhasil',
+            'user' => $user
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+
+            return response()->json([
+                'message' => 'Login gagal'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Login berhasil',
+            'user' => $user
+        ]);
+    }
+}
