5759de372547bf7f8a9e841095d45f8c3e737781
dedy-wiratmono <demy.muda02965@gmail.com>
2026-05-18 17:28:22 +0700

    Update and rename welcome.blade.php to login.blade.php
    ui login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>SMART TEST</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <div class="card">
+        <h1>SMART TEST</h1>
+        <p>Login Mahasiswa</p>
+
+        <input type="text" placeholder="NIM">
+        <input type="password" placeholder="Password">
+
+        <button>LOGIN</button>
+
+        <a href="#">Belum punya akun?</a>
+    </div>
+</div>
+
+</body>
+</html>
