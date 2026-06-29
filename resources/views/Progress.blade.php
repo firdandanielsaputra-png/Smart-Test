fbfda2e203985e30d0dc98c94e63d3b4dd6a7f9f
Shafira <sucimarv@gmail.com>
2026-06-29 14:40:58 +0700

    Create Progress.blade.php
    #Shafira

---
+++ resources/views/Progress.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Progress</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+<div class="card">
+
+<h1>Progress Belajar</h1>
+
+<p>Total Quiz : 10</p>
+<p>Level : Advance</p>
+<p>Rata-rata Nilai : 88</p>
+
+</div>
+
+</div>
+
+</body>
+</html>
