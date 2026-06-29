7ccae8db3e93523d472dee6cc3f72e3cba4a331b
Shafira <sucimarv@gmail.com>
2026-06-29 14:36:52 +0700

    Add HasilQuiz view for displaying quiz results
    #Shafira

---
+++ resources/views/HasilQuiz.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hasil Quiz</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+<div class="card">
+
+<h1>Hasil Quiz</h1>
+
+<p>Nilai : 85</p>
+<p>Level : Intermediate</p>
+<p>Rekomendasi : Machine Learning Dasar</p>
+
+<a href="/progress">
+<button>Lihat Progress</button>
+</a>
+
+</div>
+
+</div>
+
+</body>
+</html>
