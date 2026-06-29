1242d4c14fe6b2c6eaa03377f41145319dbf2444
Shafira <sucimarv@gmail.com>
2026-06-29 14:32:55 +0700

    quiz
    #shafira

---
+++ resources/views/quiz.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quiz</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<div class="container">
+
+<div class="card">
+
+<h2>Soal 1</h2>
+
+<p>Apa kepanjangan HTML?</p>
+
+<input type="radio"> Hyper Text Markup Language <br><br>
+<input type="radio"> Hyper Transfer Machine Language <br><br>
+<input type="radio"> High Text Machine Language <br><br>
+
+<button>Next</button>
+
+</div>
+
+</div>
+
+</body>
+</html>
