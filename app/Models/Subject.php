974fb31bec56115db39f6afe377f48747c7e6bd0
heerosupply <aljurumitamim@gmail.com>
2026-06-30 18:33:54 +0700

    Create Subject.php

---
+++ app/Models/Subject.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'kode',
+        'nama',
+        'deskripsi'
+    ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function results()
+    {
+        return $this->hasMany(Result::class);
+    }
+
+    public function recommendations()
+    {
+        return $this->hasMany(Recommendation::class);
+    }
+}
