1007c29d73395e90002f8ae995eabc3813690d96
heerosupply <aljurumitamim@gmail.com>
2026-07-02 10:02:18 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,17 +10,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable([
+    'name',
+    'email',
+    'password'
+])]
+
+#[Hidden([
+    'password',
+    'remember_token'
+])]
+
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * Cast Attributes
      */
     protected function casts(): array
     {
@@ -28,5 +35,29 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * User memiliki banyak hasil quiz
+     */
+    public function results()
+    {
+        return $this->hasMany(Result::class);
+    }
+
+    /**
+     * User memiliki satu progress
+     */
+    public function progress()
+    {
+        return $this->hasOne(Progress::class);
+    }
+
+    /**
+     * User memiliki banyak rekomendasi
+     */
+    public function recommendations()
+    {
+        return $this->hasMany(Recommendation::class);
     }
 }
